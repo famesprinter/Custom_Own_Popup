8e54e92381053786a1bbdde16a1ce103cd4929f8
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-04-29 14:55:18 +0700

    - Init custom popup project.

---
+++ CustomPopUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomPopUp
+//
+//  Created by Kittitat Rodphotong on 4/29/2559 BE.
+//  Copyright © 2559 FameSprinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
